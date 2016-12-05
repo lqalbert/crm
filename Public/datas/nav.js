3e1f90899d185a01c554f8ad746f19178f1c9e44
hujianjian <huj@riign.com>
2016-12-05 17:20:28 +0800

    修改了部分CSS

---
+++ Public/datas/nav.js
@@ -0,0 +1,142 @@
+var navs = [{
+	"title": "百宝箱",
+	"icon": "fa-cubes",
+	"spread": false,
+	"children": [{
+		"title": "我的工作台",
+		"icon": "&#xe641;",
+		"href": "#",
+	}, {
+		"title": "消息盒子",
+		"icon": "&#xe63c;",
+		"href": "#"
+	}, {
+		"title": "系统公告",
+		"icon": "&#xe63c;",
+		"href": "#"
+	}, {
+		"title": "集思信箱",
+		"icon": "&#xe609;",
+		"href": "#"
+	}, {
+		"title": "工作总结",
+		"icon": "&#xe62a;",
+		"href": "#"
+	}, {
+		"title": "培训学院",
+		"icon": "&#xe60c;",
+		"href": "#"
+	}, {
+		"title": "素材库",
+		"icon": "&#xe60c;",
+		"href": "#"
+	}, {
+		"title": "系统管理",
+		"icon": "&#xe60c;",
+		"href": "#"
+	}]
+}, {
+	"title": "CPS模块",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "短网址URL",
+		"icon": "fa-table",
+		"href": "#"
+	}]
+}, {
+	"title": "组织员工",
+	"icon": "&#x1002;",
+	"spread": false,
+	"children": [{
+		"title": "组织单位",
+		"icon": "fa-check-square-o",
+		"href": "#"
+	},{
+		"title": "团队小组",
+		"icon": "fa-check-square-o",
+		"href": "#"
+	},{
+		"title": "员工管理",
+		"icon": "fa-check-square-o",
+		"href": "#"
+	},{
+		"title": "角色管理",
+		"icon": "fa-check-square-o",
+		"href": "#"
+	},{
+		"title": "权限管理",
+		"icon": "fa-check-square-o",
+		"href": "#"
+	}]
+}, {
+	"title": "日常工作",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "客户管理",
+		"icon": "fa-github",
+		"href": "#"
+	}, {
+		"title": "资料接收",
+		"icon": "fa-qq",
+		"href": "#"
+	}]
+},{
+	"title": "统计报表",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "量化报表",
+		"icon": "fa-github",
+		"href": "#"
+	}, {
+		"title": "客户报表",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "客户统计",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "业绩报表",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "续费报表",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "业绩排名",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "资料跟踪",
+		"icon": "fa-qq",
+		"href": "#"
+	}]
+},{
+	"title": "业务助手",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "Q群扫描",
+		"icon": "fa-github",
+		"href": "#"
+	}, {
+		"title": "手机预查",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "异常IP",
+		"icon": "fa-qq",
+		"href": "#"
+	}, {
+		"title": "回收站",
+		"icon": "fa-qq",
+		"href": "#"
+	}]
+}];
