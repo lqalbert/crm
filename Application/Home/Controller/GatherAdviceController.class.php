7fda8f9b882b9bac0f29045ed0a1354e2af2270d
hujianjian <huj@riign.com>
2017-02-28 10:08:52 +0800

    阶段二部分界面

---
+++ Application/Home/Controller/GatherAdviceController.class.php
@@ -0,0 +1,37 @@
+<?php
+namespace Home\Controller;
+
+class GatherAdviceController extends CommonController{
+  protected $pageSize = 11;
+  
+  public function index(){
+  	$this->display();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Application/Home/Controller/WorkSummaryController.class.php
@@ -0,0 +1,37 @@
+<?php
+namespace Home\Controller;
+
+class WorkSummaryController extends CommonController{
+  protected $pageSize = 11;
+  
+  public function index(){
+  	$this->display();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
