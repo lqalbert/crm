9c164d9e3f2832777ce14fe38205fc0c5651b90d
hujianjian <huj@riign.com>
2017-05-24 16:25:19 +0800

    修改button提示语 修改添加和显示投诉记录问题

--- Application/Home/Controller/CommonHandleLogicController.class.php
+++ Application/Home/Controller/CommonHandleLogicController.class.php
@@ -53,7 +53,6 @@ class CommonHandleLogicController extends CommonController{
   *
   */
   public function addComplain(){
-    
     $LogM = D('CustomerLog');
     $cusM = D('Customer');
     $msgBox = D('MsgBox');
@@ -71,8 +70,8 @@ class CommonHandleLogicController extends CommonController{
         return L('ADD_ERROR').$LogM->getError();
     }
 
-   // $cusM ->find($LogM->cus_id);
-    $cusM ->field('type')->find($LogM->cus_id);
+    //$cusM ->field('type')->find($LogM->cus_id);
+    $cusM->find($LogM->cus_id);
 
     if ($to_type !== "" &&  $to_type != I('post.type')) {
         $LogM->contentSetChangeType(I('post.type'), $to_type);
