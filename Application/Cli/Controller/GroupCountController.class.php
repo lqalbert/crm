739f4875a7e9c94cd750134a67982d49568538ae
hujianjian <huj@riign.com>
2017-05-27 16:38:27 +0800

    修改登录界面样式

--- Application/Cli/Controller/GroupCountController.class.php
+++ Application/Cli/Controller/GroupCountController.class.php
@@ -7,15 +7,13 @@ use Cli\Service\GroupCountServiceModel;
 /**
 * 在 customerCount 完了之后再 运行统计
 */
-
-
 class GroupCountController extends Controller{
 
     private $date = '';
     private $m = '';
     public function index($date){
-        $this->date = $date;
-        $this->m = new GroupCountServiceModel;
-        return $this->m->index($this->date);
+      $this->date = $date;
+      $this->m = new GroupCountServiceModel;
+      return $this->m->index($this->date);
     }
 }
