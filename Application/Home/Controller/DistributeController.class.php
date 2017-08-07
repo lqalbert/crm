168a389c4ae996f94282724fc24fddcaca77593f
每次都要取昵称 <hyfdyx@qq.com>
2017-08-07 13:46:31 +0800

    临时换分支改bug

--- Application/Home/Controller/DistributeController.class.php
+++ Application/Home/Controller/DistributeController.class.php
@@ -7,9 +7,19 @@ class DistributeController extends CommonController{
 
     protected $table = "Distribute";
 
+
     public function index(){
 
         $this->assign("types", DistributeModel::$type);
         $this->display();   
+    }
+
+    public function edit(){
+        
+    }
+
+    public function view(){
+        $this->assign("pageSize", 0);
+        $this->display("_view");
     }
 }
