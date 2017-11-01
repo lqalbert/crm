3908efe5138cf278035f38225c0b6341a1e841c4
hujianjian <hujian_gosu@163.com>
2017-11-01 17:43:37 +0800

    20171101重构二维码管理模块

--- Application/Home/Controller/QrCodeManageController.class.php
+++ Application/Home/Controller/QrCodeManageController.class.php
@@ -4,30 +4,14 @@ namespace Home\Controller;
 class QrCodeManageController extends CommonController{
 
   public function index(){
-  	$this->assign('res',$this->getRes());
+  	$this->assign('res',$this->getQr());
   	$this->display();
   }
 
-  protected function getRes(){
+  protected function getQr(){
     $user_id = session('uid');
-    $qrArr = M('user_info')->where(array('user_id'=>$user_id))->field("qqqr,wxqr")->select();
-    $qrArr = reset($qrArr);
-    if($qrArr['qqqr'] && $qrArr['wxqr']){
-      $res['qqqrImg'] = __ROOT__.$qrArr['qqqr'];
-      $res['wxqrImg'] = __ROOT__.$qrArr['wxqr'];
-    }else if(!$qrArr['qqqr'] && $qrArr['wxqr']){
-      $res['qqqrImg'] = $qrArr['qqqr'];
-      $res['wxqrImg'] = __ROOT__.$qrArr['wxqr'];
-    }else if($qrArr['qqqr'] && !$qrArr['wxqr']){
-      $res['qqqrImg'] = __ROOT__.$qrArr['qqqr'];
-      $res['wxqrImg'] = $qrArr['wxqr'];
-    }else{
-      $res['qqqrImg'] = $qrArr['qqqr'];
-      $res['wxqrImg'] = $qrArr['wxqr'];
-    }
-    return $res;
-
-    
+    $qrArr = M('user_info')->where(array('user_id'=>$user_id))->field("qqqr,wxqr")->find();
+    return $qrArr;  
   }
 
 
--- Application/Home/Controller/UploadController.class.php
+++ Application/Home/Controller/UploadController.class.php
@@ -62,36 +62,33 @@ class UploadController extends CommonController {
 
     //处理微信和QQ二维码截图裁图缩图
     public function imageCropperUpload($folder,$authSub=true){
-        $upload = new \Think\Upload();// 实例化上传类
-        $upload->maxSize   =     3145728 ;// 设置附件上传大小3M
-        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $upload->rootPath  =     './Upload/'.$folder.'/'; // 设置附件上传根目录
-        $upload->autoSub   =     $authSub;
-        $info   = $upload->upload();
-        if(!$info) {// 上传错误提示错误信息
-    			echo $upload->getError();
-        }else{// 上传成功
-        	$user_id=session('uid');
-        	foreach ($info as $file) {
-        		$qrPath =substr($upload->rootPath, 1 ).$file['savepath'].$file['savename'];
-        		$data=array($folder=>$qrPath);
-        	}
-    			$image = new \Think\Image(); 
-    			$image->open('./'.$qrPath);//
-    			$image->thumb(I('post.maxW'),I('post.maxH'))->save('./'.$qrPath);
-    			//将图片裁剪为200x200并保存为$qrPath
-    			$image->open('./'.$qrPath);
-    			$image->crop(I('post.w'), I('post.h'),I('post.x1'),I('post.y1'))->save('./'.$qrPath);
-        	$res=M('user_info')->where(array('user_id'=>$user_id))->save($data);
-        	if($res){
-       			$_SESSION['account']['userInfo'][$folder]=$data[$folder];
-       			echo $qrPath;
-        	}else{
-       			echo "上传失败，请稍后再试！";
-        	}
-        }
+      $upload = new \Think\Upload();// 实例化上传类
+      $upload->maxSize   =     3145728 ;// 设置附件上传大小3M
+      $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+      $upload->rootPath  =     './Upload/'.$folder.'/'; // 设置附件上传根目录
+      $upload->autoSub   =     $authSub;
+      $info   = $upload->upload();
+      if(!$info) {// 上传错误提示错误信息
+        $this->error($upload->getError());
+      }else{// 上传成功
+      	$user_id=session('uid');
+      	foreach ($info as $file) {
+      		$qrPath =substr($upload->rootPath, 1 ).$file['savepath'].$file['savename'];
+      		$data=array($folder=>$qrPath);
+      	}
+  			$image = new \Think\Image(); 
+  			//将图片裁剪为选择的尺寸并保存为$qrPath
+  			$image->open('./'.$qrPath);
+  			$image->crop(I('post.w'), I('post.h'),I('post.x'),I('post.y'))->save('./'.$qrPath);
+      	$res=M('user_info')->where(array('user_id'=>$user_id))->save($data);
+      	if($res){
+     			$_SESSION['account']['userInfo'][$folder]=$data[$folder];
+          $this->ajaxReturn(array('info'=>'二维码上传成功！','path'=>$qrPath,'folder'=>$folder));
+      	}else{
+          $this->error($this->M->getError());
+      	}
+      }
     }
-
 
 
   //处理推广素材页Ueditor上传图片
@@ -150,77 +147,53 @@ class UploadController extends CommonController {
       echo $result;  
     }  
  
-
-
-
-
   }
 
+  //百度UMeditor上传图片 只实现上传图片的功能
+  // 返回格式
+  // {
+  //     "originalName":"Chrysanthemum.jpg",
+  //     "name":"15048371185276.jpg",
+  //     "url":"upload\/20170908\/15048371185276.jpg",
+  //     "size":879394,
+  //     "type":".jpg",
+  //     "state":"SUCCESS"
+  // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-    //百度UMeditor上传图片 只实现上传图片的功能
-    // 返回格式
-    // {
-    //     "originalName":"Chrysanthemum.jpg",
-    //     "name":"15048371185276.jpg",
-    //     "url":"upload\/20170908\/15048371185276.jpg",
-    //     "size":879394,
-    //     "type":".jpg",
-    //     "state":"SUCCESS"
-    // }
-
-    // {"upfile":
-    //     {
-    //         "name":"Jellyfish.jpg",
-    //         "type":"image\/jpeg",
-    //         "size":775702,
-    //         "key":"upfile",
-    //         "ext":"jpg",
-    //         "md5":"5a44c7ba5bbe4ec867233d67e4806848",
-    //         "sha1":"3b15be84aff20b322a93c0b9aaa62e25ad33b4b4",
-    //         "savename":"59b203f7d6038.jpg",
-    //         "savepath":""
-    //     }
-    // }
-    public function uploadUM(){
-        $this->upload = new \Think\Upload();// 实例化上传类
-        $this->upload->maxSize   =     3145728 ;// 设置附件上传大小3M
-        $this->upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-        $this->upload->rootPath  =     './Upload/'; // 设置附件上传根目录
-        $this->upload->autoSub   =     true;
-        $info =  $this->upload->upload();
-        if(!$info) {// 上传错误提示错误信息
-            $this->error(array('state'=>'ERROR'));
-        }else{// 上传成功
-            $re = array(
-                'originalName'=>$info['upfile']['name'],
-                'name'        =>$info['upfile']['savename'],
-                'url'         =>__ROOT__.substr($this->upload->rootPath, 1 ).$info['upfile']['savepath'].$info['upfile']['savename'],
-                'size'        =>$info['upfile']['size'],
-                'type'        =>"." . $info['upfile']['ext'],
-                'state'       =>'SUCCESS'
-                );
-            $this->ajaxReturn(json_encode($re), 'EVAL');
-        }
+  // {"upfile":
+  //     {
+  //         "name":"Jellyfish.jpg",
+  //         "type":"image\/jpeg",
+  //         "size":775702,
+  //         "key":"upfile",
+  //         "ext":"jpg",
+  //         "md5":"5a44c7ba5bbe4ec867233d67e4806848",
+  //         "sha1":"3b15be84aff20b322a93c0b9aaa62e25ad33b4b4",
+  //         "savename":"59b203f7d6038.jpg",
+  //         "savepath":""
+  //     }
+  // }
+  public function uploadUM(){
+    $this->upload = new \Think\Upload();// 实例化上传类
+    $this->upload->maxSize   =     3145728 ;// 设置附件上传大小3M
+    $this->upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+    $this->upload->rootPath  =     './Upload/'; // 设置附件上传根目录
+    $this->upload->autoSub   =     true;
+    $info =  $this->upload->upload();
+    if(!$info) {// 上传错误提示错误信息
+      $this->error(array('state'=>'ERROR'));
+    }else{// 上传成功
+      $re = array(
+        'originalName'=>$info['upfile']['name'],
+        'name'        =>$info['upfile']['savename'],
+        'url'         =>__ROOT__.substr($this->upload->rootPath, 1 ).$info['upfile']['savepath'].$info['upfile']['savename'],
+        'size'        =>$info['upfile']['size'],
+        'type'        =>"." . $info['upfile']['ext'],
+        'state'       =>'SUCCESS'
+        );
+      $this->ajaxReturn(json_encode($re), 'EVAL');
     }
+  }
 
 
 
