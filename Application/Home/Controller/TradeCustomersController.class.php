009b2afbee5dbc90f0ede51ba0ee465518dfe68b
hujianjian <hujian_gosu@163.com>
2017-11-03 10:58:33 +0800

    20171103更新前暂存

---
+++ Application/Home/Controller/TradeCustomersController.class.php
@@ -0,0 +1,49 @@
+<?php
+namespace Home\Controller;
+use Home\Model\CustomerLogModel;
+use Home\Model\CustomerModel;
+use Home\Model\DepartmentModel;
+use Home\Model\RoleModel;
+use Common\Lib\User;
+
+class TradeCustomersController extends CommonController{
+	protected $pageSize = 15;
+
+	public function index(){
+
+  
+
+    $this->display();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
